79d7d01023151e6983ef987fde76575bb5a4fa7a
Nikolas Gianoglou <nikolasgianoglou@Nikolass-MacBook-Air.local>
2024-03-15 08:47:59 -0300

    Add ContentViewModel to handle requests from  About, terms and privacy

---
+++ FreeLife/FreeLife/Profile/ProfileScreens/ViewModel/ContentViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  ContentViewModel.swift
+//  FreeLife
+//
+//  Created by Nikolas Gianoglou on 15/03/24.
+//
+
+import Foundation
+import Alamofire
+
+struct Content: Codable {
+    var id: Int
+    var type: String
+    var content: String
+}
+
+enum ContentType: String {
+    case about = "ABOUT"
+    case terms = "TERMS"
+    case privace = "PRIVACE"
+}
+
+class ContentViewModel {
+    
+    var dataSource: Content
+    
+    init(dataSource: Content) {
+        self.dataSource = dataSource
+    }
+    
+    func getContent(type: ContentType, completion: @escaping (Result<Content, DomainError>) -> Void) {
+        
+        guard let url = URL(string:"https://freelifeconect.app.br:8080/texts") else { return }
+        let body: Parameters = [
+            "type": type.rawValue
+        ]
+        
+        Service.shared.request(in: url, method: .get, parameters: body, encoding: URLEncoding.queryString) { result in
+            switch result {
+            case .success(let data):
+                if let data = data, let root = try? JSONDecoder().decode(Content.self, from: data) {
+                    self.dataSource = root
+                    completion(.success(root))
+                } else {
+                    completion(.failure(.unexpected))
+                }
+            case .failure(let error):
+                switch error {
+                case .noConnectivity:
+                    completion(.failure(.noConnectivity))
+                case let .badRequest(data):
+                    if let data = data, let dictionary = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                        completion(.failure(.unauthorized(message: dictionary["error"] as? String)))
+                    }
+                default:
+                    completion(.failure(.unexpected))
+                }
+            }
+        }
+    }
+}
